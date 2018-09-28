3d7cc691bd23fbba7471b4d0f2ceea5c085ade2d
chenyuhan1 <1944614333@qq.com>
2018-09-28 22:29:41 +0800

    markman和数据类型

---
+++ js/type_test/1.js
@@ -0,0 +1,28 @@
+var und = undefined;
+var nul = null;
+var boo = true;
+var num = 1;
+var str = 'xys';
+// Sybmol
+
+// 复杂数据类型 引用类型
+var obj = {a: 1};
+var obj1 = {};
+var func = function(){};
+var arr = [1, 2, 3];
+// var obj1 = obj;
+// obj1.a = 2;
+// console.log(obj);
+// console.log('--------');
+// console.log(obj1);
+// for(var key in obj){
+//     obj1[key] = obj[key];
+// }
+// obj1.a = 2;
+// console.log(obj);
+// console.log(obj1);
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(und));
+console.log(typeof(func));
+console.log(typeof(arr));
