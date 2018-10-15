c8755757d96adb273c48ebc47b65da8839853773
chenyuhan1 <1944614333@qq.com>
2018-10-15 16:57:44 +0800

    test

---
+++ tpyescript/type.ts
@@ -0,0 +1,16 @@
+// 布尔值
+let isDone: boolean = false;
+isDone = true;
+
+// 数字
+let decLiteral: number = 6;
+decLiteral = 7;
+let hexLiteral: number = 0xf00d;
+let binary: number = 0b1010;
+let octalLiteral = 0o744;
+
+let nicjname: string = "boob";
+let age: number = 37;
+let sentence: string = `Hello, myname is ${nicjname}
+I'll be ${age++} yes old next month`;
+let list: number[] = [1, 2, 3];
