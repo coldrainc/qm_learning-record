f5fdaacd436a74264c135d681dd8f943f7544ea8
chenyuhan1 <1944614333@qq.com>
2018-10-15 18:06:30 +0800

    â错误

---
+++ WXApp/pages/stories/index.js
@@ -0,0 +1,12 @@
+/*use 'esversion: 6'*/
+const app = getApp();
+
+Page({
+    data: {
+
+    },
+    // 声明周期函数
+    // onLoad (option) {
+    //     console.log('onload');
+    // }
+});
---
+++ tpyescript/try.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person + " 你最近又胖了";
+}
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
